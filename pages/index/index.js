392a57fd5cddef1ca1447e32e45fe626c1be4585
zhouyaling <yaling618@126.com>
2020-11-23 09:06:40 +0800

    初始化

---
+++ pages/index/index.js
@@ -0,0 +1,99 @@
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+const user = require('../../services/user.js');
+
+//获取应用实例
+const app = getApp()
+Page({
+    data: {
+        newGoods: [],
+        hotGoods: [],
+        topics: [],
+        brands: [],
+        floorGoods: [],
+        banner: [],
+        channel: []
+    },
+    onShareAppMessage: function () {
+        return {
+            title: '万千风物满店香,为君聚来细品尝',
+            desc: '风物聚',
+            path: '/pages/index/index'
+        }
+    }, onPullDownRefresh() {
+        // 增加下拉刷新数据的功能
+        var self = this;
+        this.getIndexData();
+    },
+    getIndexData: function () {
+        let that = this;
+        var data = new Object();
+        // 新品收发
+        util.request(api.IndexUrlNewGoods).then(function (res) {
+            if (res.errno === 0) {
+                data.newGoods = res.data.newGoodsList
+                that.setData(data);
+            }
+        });
+        // 人气推荐
+        util.request(api.IndexUrlHotGoods).then(function (res) {
+            if (res.errno === 0) {
+                data.hotGoods = res.data.hotGoodsList
+                that.setData(data);
+            }
+        });
+        // 发现精选
+        // util.request(api.IndexUrlTopic).then(function (res) {
+        //     if (res.errno === 0) {
+        //         data.topics = res.data.topicList
+        //         that.setData(data);
+        //     }
+        // });
+
+        // 品牌直供
+        util.request(api.IndexUrlBrand).then(function (res) {
+            if (res.errno === 0) {
+                data.brand = res.data.brandList
+                that.setData(data);
+            }
+        });
+        // // 分类
+        // util.request(api.IndexUrlCategory).then(function (res) {
+        //     if (res.errno === 0) {
+        //         data.floorGoods = res.data.categoryList
+        //         that.setData(data);
+        //     }
+        // });
+        // 轮播图
+        util.request(api.IndexUrlBanner).then(function (res) {
+
+            if (res.errno === 0) {
+                data.banner = res.data.banner
+                that.setData(data);
+            }
+        });
+        // 分类栏目
+        util.request(api.IndexUrlChannel).then(function (res) {
+            if (res.errno === 0) {
+                data.channel = res.data.channel
+                that.setData(data);
+            }
+        });
+
+    },
+    onLoad: function (options) {
+        this.getIndexData();
+    },
+    onReady: function () {
+        // 页面渲染完成
+    },
+    onShow: function () {
+        // 页面显示
+    },
+    onHide: function () {
+        // 页面隐藏
+    },
+    onUnload: function () {
+        // 页面关闭
+    },
+})
